08496f98b12dd76b1d0b9faef96f1b360f41ee89
cj <2012cjz@gmail.com>
2017-06-21 15:08:41 +0900

    aja

--- str/bottom.js
+++ str/bottom.js
@@ -17,4 +17,4 @@ for(var i=Object.keys(dcss).length;i>=1;i--){
 	str = '<article class="container"><div class="row"><picture class="col-md-3"><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"><img src="'+dcss[Object.keys(dcss)[i - 1]].img+'" alt="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></picture><div class="col-md-9"><h2><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></h2></div></div></article>';
 	arr += str;
 }
-$('main').html('arr')
+$('main').html(arr);
