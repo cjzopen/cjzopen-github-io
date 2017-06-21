8e7f48dd940dbdcc0927b11f6ed9696af47a1545
cj <2012cjz@gmail.com>
2017-06-21 16:13:31 +0900

    0621pm

--- str/bottom.js
+++ str/bottom.js
@@ -1,3 +1,4 @@
+var $main = $('main');
 var noImg ='/img/rrr.jpg';
 var dcss = {
     a1:{
@@ -15,9 +16,11 @@ var dcss = {
         "preface": "test preface"
     }
 }
-var arr='';
+var hcss='';
 var str='';
 for(var i=Object.keys(dcss).length;i>=1;i--){
 	str = '<article class="container"><div class="row"><picture class="col-sm-3"><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"><img class="img-responsive center-block" src="'+dcss[Object.keys(dcss)[i - 1]].img+'" alt="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></picture><div class="col-sm-9"><h2 class="main-color"><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'">'+dcss[Object.keys(dcss)[i - 1]].title+'</a></h2><time class="main-color" datetime="'+dcss[Object.keys(dcss)[i - 1]].date+'">'+dcss[Object.keys(dcss)[i - 1]].date+'</time><p>'+dcss[Object.keys(dcss)[i - 1]].preface+'</p></div></div></article>';
-	arr += str;
+	hcss += str;
 }
+$main.html('<section class="container"></section>');
+$main.find('section').html(hcss);
