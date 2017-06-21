e0d7844531e85850a44a04e0418f96af656a4096
cj <2012cjz@gmail.com>
2017-06-21 15:07:58 +0900

    aja

--- str/bottom.js
+++ str/bottom.js
@@ -1,19 +1,20 @@
-
+var noImg ='/img/rrr.jpg';
 var dcss = {
     a1:{
-        "img": "jimmy",
-        "link": "hello world",
-        "title": "aa"
+        "img": noImg,
+        "link": "#",
+        "title": "sticky"
     },
     a2:{
-        "img": "billy",
-        "link": "foo equals bar",
-        "title": "aa"
+        "img": noImg,
+        "link": "#",
+        "title": "test"
     }
 }
-var data='';
+var arr='';
 var str='';
 for(var i=Object.keys(dcss).length;i>=1;i--){
 	str = '<article class="container"><div class="row"><picture class="col-md-3"><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"><img src="'+dcss[Object.keys(dcss)[i - 1]].img+'" alt="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></picture><div class="col-md-9"><h2><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></h2></div></div></article>';
-	data += str;
+	arr += str;
 }
+$('main').html('arr')
