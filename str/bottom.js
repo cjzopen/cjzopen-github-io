d10fb46cbc20baa28a0b472056a91d95690b4078
cj <2012cjz@gmail.com>
2017-06-21 15:37:25 +0900

    0621pm

--- str/bottom.js
+++ str/bottom.js
@@ -21,5 +21,3 @@ for(var i=Object.keys(dcss).length;i>=1;i--){
 	str = '<article class="container"><div class="row"><picture class="col-sm-3"><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"><img class="img-responsive center-block" src="'+dcss[Object.keys(dcss)[i - 1]].img+'" alt="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></picture><div class="col-sm-9"><h2 class="main-color"><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'">'+dcss[Object.keys(dcss)[i - 1]].title+'</a></h2><time class="main-color" datetime="'+dcss[Object.keys(dcss)[i - 1]].date+'">'+dcss[Object.keys(dcss)[i - 1]].date+'</time><p>'+dcss[Object.keys(dcss)[i - 1]].preface+'</p></div></div></article>';
 	arr += str;
 }
-$('main').html('<section class="container"></section>')
-$('main section').html(arr);
