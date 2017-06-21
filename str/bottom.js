176ce177de9a4247d4c5eca823e042c319a669c7
cj <2012cjz@gmail.com>
2017-06-21 15:14:16 +0900

    aja

--- str/bottom.js
+++ str/bottom.js
@@ -14,7 +14,8 @@ var dcss = {
 var arr='';
 var str='';
 for(var i=Object.keys(dcss).length;i>=1;i--){
-	str = '<article class="container"><div class="row"><picture class="col-md-3"><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"><img src="'+dcss[Object.keys(dcss)[i - 1]].img+'" alt="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></picture><div class="col-md-9"><h2><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></h2></div></div></article>';
+	str = '<article class="container"><div class="row"><picture class="col-sm-3"><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"><img class="img-responsive center-block" src="'+dcss[Object.keys(dcss)[i - 1]].img+'" alt="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></picture><div class="col-sm-9"><h2><a href="'+dcss[Object.keys(dcss)[i - 1]].link+'" title="'+dcss[Object.keys(dcss)[i - 1]].title+'"></a></h2></div></div></article>';
 	arr += str;
 }
-$('main').html(arr);
+$('main').html('<section class="container"></section>')
+$('main section').html(arr);
