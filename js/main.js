89246776d8ec375252c79c828a588e1a72ade668
cj <2012cjz@gmail.com>
2017-06-28 17:43:24 +0900

    0628pm

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $(function(){
 	$('.loading').fadeOut()
-	if($('#gp').length){
+
+	var googleShare = $('#gp');
+	if(googleShare.length){
 		var clink = window.location.href;
-		$('#gp').attr('href','https://plus.google.com/share?url='+clink);
-		$('#gp').html('<img src="https://www.gstatic.com/images/icons/gplus-32.png" alt="Share on Google+">').end().click(function(){
+		googleShare.attr('href','https://plus.google.com/share?url='+clink);
+		googleShare.html('<img src="https://www.gstatic.com/images/icons/gplus-32.png" alt="Share on Google+" style="width:28px;vertical-align:bottom">')
+		googleShare.on('click',function(){
 			window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=360');
 			return false;
 		})
