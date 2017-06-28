2894ec0617a923a8499a653f3bb1f6e933e3f4d4
cj <2012cjz@gmail.com>
2017-06-28 17:39:32 +0900

    0628pm

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $(function(){
 	$('.loading').fadeOut()
 	if($('#gp').length){
 		var clink = window.location.href;
-		$('#gp').attr('href','https://plus.google.com/share?url='+clink).end().html('<img src="https://www.gstatic.com/images/icons/gplus-32.png" alt="Share on Google+">').end().click(function(){
+		$('#gp').attr('href','https://plus.google.com/share?url='+clink);
+		$('#gp').html('<img src="https://www.gstatic.com/images/icons/gplus-32.png" alt="Share on Google+">').end().click(function(){
 			window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=360');
 			return false;
 		})
