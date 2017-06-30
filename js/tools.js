fba721cc0cf10a6bcd0ec623c84a4b233753a182
cj <2012cjz@gmail.com>
2017-06-30 17:07:39 +0900

    0630

--- js/tools.js
+++ js/tools.js
@@ -23,7 +23,7 @@ $(function(){
 	var htools ='';
 	var str ='';
 	for(var i=Object.keys(tools).length;i>=1;i--){
-		str = '<div class="col-xs-12 col-sm-6 col-md-3"><a class="thumbnail" href="'+tools[Object.keys(tools)[i - 1]].link+'" title="'+tools[Object.keys(tools)[i - 1]].title+'"><img src="'+img+'" alt="*"><h2 class="main-color">'+tools[Object.keys(tools)[i - 1]].title+'</h2><p>'+tools[Object.keys(tools)[i - 1]].preface+'</p></div>';
+		str = '<div class="col-xs-12 col-sm-6 col-md-3"><a class="thumbnail" href="'+tools[Object.keys(tools)[i - 1]].link+'" title="'+tools[Object.keys(tools)[i - 1]].title+'"><img src="'+img+'" alt="*"><h2 class="main-color">'+tools[Object.keys(tools)[i - 1]].title+'</h2><p class="text-left">'+tools[Object.keys(tools)[i - 1]].preface+'</p></div>';
 		htools += str;
 	}
 	$('#tool-data').html(htools);
