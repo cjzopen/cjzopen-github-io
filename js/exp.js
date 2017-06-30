df34a9d2c50dc83b5a1929d43af64a8fee91e34e
cj <2012cjz@gmail.com>
2017-06-30 16:02:08 +0900

    0630

--- js/exp.js
+++ js/exp.js
@@ -1,3 +1,11 @@
 $(function(){
-	$('header.sticky').load('/exp/_nav.html')
+	$.ajax({
+		type: "GET",
+        url: '/exp/str/nav.html',
+    }).done(function(data){
+        console.log('menu loaded')
+        nav = data;
+        $('header.sticky').html(nav)
+	})
+	// $('header.sticky').load('/exp/str/nav.html')
 })
